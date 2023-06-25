5bdb7992befea1c3b9c14a3f88b8894eed29643f
ArtemiyKew <hezam228gg@gmail.com>
2023-06-25 13:43:12 +0400

    chapter 1, basics golang

---
+++ chapter_1/main.go
@@ -0,0 +1,23 @@
+/*
+Первая программа
+на языке Go
+*/
+package main // определение пакета для текущего файла
+import "fmt" // подключение пакета fmt
+
+// определение функции main
+func main() {
+	fmt.Println("Hello world!") // вывод строки на консоль
+
+	var i int
+	var a, b, c string
+	var hello string = "Hello"
+	var (
+		name    string = "Vasya"
+		surName string = "Petrov"
+	)
+
+	age := 10
+	fmt.Println(i, a, b, c, hello, name, surName, "age =", age)
+	TestDataTypes()
+}
