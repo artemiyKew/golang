f75648b5d4464b888774f789e636eedb11ca1a81
Artemiy Savinov <flartemiy@gmail.com>
2023-10-12 00:44:15 +0400

    repository pattern

--- architecture/cmd/main.go
+++ architecture/cmd/main.go
@@ -1,7 +1,83 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 func main() {
-	fmt.Println("Hello World!")
+	repo := &InMemoryUserRepository{}
+	user := &User{
+		Name:     "John",
+		Email:    "john@example.com",
+		Password: "secret",
+	}
+	repo.Create(user)
+	fmt.Println(repo.GetAll())
+	user.Name = "Jane"
+	repo.Update(user)
+	fmt.Println(repo.GetAll())
+	repo.Delete(user.ID)
+	fmt.Println(repo.GetAll())
+}
+
+// repository.go
+type User struct {
+	ID       int
+	Name     string
+	Email    string
+	Password string
+}
+
+type UserInterface interface {
+	GetByID(id int) (*User, error)
+	GetAll() ([]*User, error)
+	Create(user *User) error
+	Update(user *User) error
+	Delete(id int) error
+}
+
+// in_memory.go
+
+type InMemoryUserRepository struct {
+	users []*User
+}
+
+func (r *InMemoryUserRepository) GetByID(id int) (*User, error) {
+	for _, user := range r.users {
+		if user.ID == id {
+			return user, nil
+		}
+	}
+	return nil, errors.New("users not found")
+}
+
+func (r *InMemoryUserRepository) GetAll() ([]*User, error) {
+	return r.users, nil
+}
+
+func (r *InMemoryUserRepository) Create(user *User) error {
+	user.ID = len(r.users) + 1
+	r.users = append(r.users, user)
+	return nil
+}
+
+func (r *InMemoryUserRepository) Update(user *User) error {
+	for i, u := range r.users {
+		if u.ID == user.ID {
+			r.users[i] = user
+			return nil
+		}
+	}
+	return errors.New("user not found")
+}
+
+func (r *InMemoryUserRepository) Delete(id int) error {
+	for i, user := range r.users {
+		if user.ID == id {
+			r.users = append(r.users[:i], r.users[i+1:]...)
+			return nil
+		}
+	}
+	return errors.New("user not found")
 }
--- architecture/readme.md
+++ architecture/readme.md
@@ -142,3 +142,69 @@ _Репозиторий_ (Repository) - это популярный подход
         Delete(id int) error
     }
 ```
+
+```golang 
+// in_memory.go
+
+type InMemoryUserRepository struct {
+	users []*User
+}
+
+func (r *InMemoryUserRepository) GetByID(id int) (*User, error) {
+	for _, user := range r.users {
+		if user.ID == id {
+			return user, nil
+		}
+	}
+	return nil, errors.New("users not found")
+}
+
+func (r *InMemoryUserRepository) GetAll() ([]*User, error) {
+	return r.users, nil
+}
+
+func (r *InMemoryUserRepository) Create(user *User) error {
+	user.ID = len(r.users) + 1
+	r.users = append(r.users, user)
+	return nil
+}
+
+func (r *InMemoryUserRepository) Update(user *User) error {
+	for i, u := range r.users {
+		if u.ID == user.ID {
+			r.users[i] = user
+			return nil
+		}
+	}
+	return errors.New("user not found")
+}
+
+func (r *InMemoryUserRepository) Delete(id int) error {
+	for i, user := range r.users {
+		if user.ID == id {
+			r.users = append(r.users[:i], r.users[i+1:]...)
+			return nil
+		}
+	}
+	return errors.New("user not found")
+}
+```
+
+```golang 
+    // main.go
+    func main() {
+	repo := &InMemoryUserRepository{}
+	user := &User{
+		Name:     "John",
+		Email:    "john@example.com",
+		Password: "secret",
+	}
+	repo.Create(user)
+	fmt.Println(repo.GetAll())
+	user.Name = "Jane"
+	repo.Update(user)
+	fmt.Println(repo.GetAll())
+	repo.Delete(user.ID)
+	fmt.Println(repo.GetAll())
+}
+```
